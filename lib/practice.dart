2f28d08b88403673c5cbc56e9a2c654599b2dbbf
Rahul <ajmerarahul042@gmail.com>
2023-07-25 22:11:46 +0530

    day3

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,16 +1,25 @@
-import "package:demo_app/home_page.dart";
+import 'package:demo_app/pages/home_page.dart';
+import 'package:demo_app/pages/login_page.dart';
 import "package:flutter/material.dart";
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: LoginPage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.pink),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        //  '/': (BuildContext context) => LoginPage()
+        // '/register' : (BuildContext context)=>LoginPage()
+      },
     );
   }
 }
